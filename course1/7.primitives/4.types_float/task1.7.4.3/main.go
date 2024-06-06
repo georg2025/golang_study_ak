44c56f349774fab62cd405c2f2c4d32972ac4ce1
George Makarov <g.makarov@automacon.ru>
2024-06-06 10:09:44 +0300

    added task 7.4.3

--- course1/7.primitives/4.types_float/task1.7.4.3/main.go
+++ course1/7.primitives/4.types_float/task1.7.4.3/main.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println(currencyPairRate("USD", "EUR", 100.0))
+	fmt.Println(CompareRoundedValues(4.66656, 4.86666, 3))
 }
 
-func currencyPairRate(a, b string, amount float64) float64 {
+func CompareRoundedValues(a, b float64, decimalPlaces int) (isEqual bool, difference float64) {
+	helpNumber := float64(math.Pow10(decimalPlaces))
+	newA := (math.Round(a * helpNumber)) / helpNumber
+	newB := (math.Round(b * helpNumber)) / helpNumber
+	if newA == newB {
+		return true, 0.0
+	}
 
+	if newA > newB {
+		return false, newA - newB
+	}
+
+	return false, newB - newA
 }
