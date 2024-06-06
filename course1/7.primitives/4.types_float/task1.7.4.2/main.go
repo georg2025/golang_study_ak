9435df34cdf407fb2a2147a645bc297101bde6bd
George Makarov <g.makarov@automacon.ru>
2024-06-06 09:55:46 +0300

    added task 7.4.2

--- course1/7.primitives/4.types_float/task1.7.4.2/main.go
+++ course1/7.primitives/4.types_float/task1.7.4.2/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/mattevans/dinero"
+)
 
 func main() {
 	fmt.Println(currencyPairRate("USD", "EUR", 100.0))
 }
 
 func currencyPairRate(a, b string, amount float64) float64 {
-
+	client := dinero.NewClient("27310dcf509547c5b5bb8932eee2ad6b", a, 20*time.Minute)
+	rates, err := client.Rates.Get(b)
+	if err != nil {
+		panic(err)
+	}
+	return amount * (*rates)
 }
