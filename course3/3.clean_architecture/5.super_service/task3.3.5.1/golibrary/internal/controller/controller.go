faf111aa86156e11faf6da41085d02771cfa439b
George Makarov <g.makarov@automacon.ru>
2024-08-01 14:06:43 +0300

    2.5 task settings

--- course3/3.clean_architecture/5.super_service/task3.3.5.1/golibrary/internal/controller/controller.go
+++ course3/3.clean_architecture/5.super_service/task3.3.5.1/golibrary/internal/controller/controller.go
@@ -1,1 +1,52 @@
 package controller
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+type UserService struct {
+	Handler http.Handler
+}
+
+func StartUserServiceWithChiRouter() *UserService {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/allusers", GetAllUsers)
+	r.Get("/allauthors", GetAllAuthors)
+	r.Get("/allbooks", GetAllBooks)
+	r.Post("/takebook", TakeBook)
+	r.Post("/returnbook", ReturnBook)
+	r.NotFound(NotFound)
+	return &UserService{Handler: r}
+}
+
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func GetAllAuthors(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func AddBook(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func GetAllBooks(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func TakeBook(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func ReturnBook(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
--- course3/3.clean_architecture/5.super_service/task3.3.5.1/golibrary/main.go
+++ course3/3.clean_architecture/5.super_service/task3.3.5.1/golibrary/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	controller "golibrary/internal/controller"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+const (
+	Address = "localhost:8080"
+)
+
+func main() {
+	handler := controller.StartUserServiceWithChiRouter()
+	server := &http.Server{
+		Addr:         Address,
+		Handler:      handler.Handler,
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 3 * time.Second,
+	}
+
+	go func() {
+		log.Println("Starting server...")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT)
+	sig := <-sigChan
+	fmt.Printf("Recieved signal: %v. Starting shutting down\n", sig)
+
+	shuttingDownTime := 5 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), shuttingDownTime)
+	defer cancel()
+
+	err := shutDown(ctx, server)
+	time.Sleep(shuttingDownTime)
+
+	if err == nil {
+		log.Println("Server stopped gracefully")
+	}
 }
